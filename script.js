f6ae0a66bf3d860fbcfee637f0a5fab11f8012e2
KLoCascio <KyndalLife@gmail.com>
2023-09-25 12:04:00 -0400

    commented out sections to be built

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+// TOP SCREEN
+const topScreen = document.querySelector(".topScreen")
+const scene = document.querySelector(".scene")
+const itemIcons = document.querySelector(".itemIcons")
+
+
+// BOTTOM SCREEN
+const bottomScreen = document.querySelector(".bottomScreen")
+const dialogue = document.querySelector(".dialogue")
+const menu = document.querySelector(".menu")
+
+//menu
+const attack = document.querySelector(".attack")
+const cast = document.querySelector(".cast")
+const items = document.querySelector(".items")
+
+// Intro Scene //
+// TOP: Needs a "Choose Your Fighter" of Rogue/Paladin/Wizard
+// Enter Name and Store Name
+/// BOTTOM: Introduction Dialogue
+/// Begin Journey Button
+
+// Travel Scene //
+// TOP
+/// BOTTOM
+
+// Combat Scene //
+// TOP
+
+/// BOTTOM
+
+// Campfire Scene //
+// TOP
+/// BOTTOM
+
+// Merchant Scene //
+// TOP
+/// BOTTOM
+
+// GameOver Scene //
+// TOP
+/// BOTTOM
+
+// Credits Scene //
+// TOP
+/// BOTTOM
+
+// End Game Conditions //
+// 1. Defeat Final Boss
+// 2. Run out of hpValue
+// 3. Hidden Ending ( === true)
+
